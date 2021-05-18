8c74808a91dce5c34817ca9109cc88378579e41f
nillkizz <kotovvsan@yandex.ru>
2021-05-18 05:24:59 +0300

    Added message storage in redis.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from aiogram import Bot, Dispatcher, executor, types
+import redis
 import settings
 
 bot = Bot(token=settings.BOT_API_TOKEN)
 dp = Dispatcher(bot)
+redis_instance = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+redis_instance.info()  # test connection
 
 
 @dp.message_handler(commands='start')
@@ -15,22 +18,28 @@ async def start_cmd_handler(message: types.Message) -> None:
 
 @dp.message_handler(content_types=types.ContentType.ANY, chat_type=types.ChatType.PRIVATE)
 async def private_chat_message_handler(message: types.Message) -> None:
-    await bot.forward_message(settings.CHAT_ID, from_chat_id=message.chat.id, message_id=message.message_id)
-    # TODO: redis.set(msg.message_id, message.chat.id)
+    msg = await bot.forward_message(settings.CHAT_ID, from_chat_id=message.chat.id, message_id=message.message_id)
+    redis_instance.set(msg.message_id, message.chat.id)
 
 
 @dp.message_handler(content_types=types.ContentType.TEXT, chat_type=types.ChatType.GROUP, is_reply=True)
 async def group_chat_text_message_handler(message: types.Message) -> None:
     if message.reply_to_message.is_forward():
-        chat_id = message.reply_to_message.forward_from.id  # TODO: chat_id = redis.get(message.reply_to_message.message_id)
-        await bot.send_message(chat_id, message.text)
+        if chat_id := redis_instance.get(message.reply_to_message.message_id):
+            await bot.send_message(int(chat_id), message.text)
+        else:
+            message_text = 'Сообщение отсутствует в базе.'
+            await bot.send_message(message.chat.id, message_text, reply_to_message_id=message.message_id)
 
 
 @dp.message_handler(content_types=types.ContentType.ANY, chat_type=types.ChatType.GROUP, is_reply=True)
 async def group_chat_any_message_handler(message: types.Message) -> None:
     if message.reply_to_message.is_forward():
-        chat_id = message.reply_to_message.forward_from.id  # TODO: chat_id = redis.get(message.reply_to_message.message_id)
-        await bot.forward_message(chat_id, from_chat_id=message.chat.id, message_id=message.message_id)
+        if chat_id := redis_instance.get(message.reply_to_message.message_id):
+            await bot.forward_message(int(chat_id), from_chat_id=message.chat.id, message_id=message.message_id)
+        else:
+            message_text = 'Сообщение отсутствует в базе.'
+            await bot.send_message(message.chat.id, message_text, reply_to_message_id=message.message_id)
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,6 +3,9 @@ from os import getenv
 BOT_API_TOKEN = getenv('BOT_API_TOKEN')
 CHAT_ID = getenv('CHAT_ID')
 
+REDIS_HOST = getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = getenv('REDIS_PORT', '6379')
+REDIS_DB = getenv('REDIS_DB', '0')
 
 required_params = ['BOT_API_TOKEN', 'CHAT_ID']
 for param in required_params:
